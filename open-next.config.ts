0739ad062b30f2be5c931eac4160ce9035ab053a
80rt0m <80rt0m@gmail.com>
2026-05-11 20:05:06 +0200

    Fix: updated open-next.config.ts for @opennextjs/cloudflare v1.x

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,29 +1,4 @@
-// default open-next.config.ts file
-import cache from "@opennextjs/cloudflare/kvCache";
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-const config = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      incrementalCache: async () => cache,
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-    },
-  },
-
-  dangerous: {
-    enableCacheInterception: false,
-  },
-};
-
-export default config;
+export default defineCloudflareConfig({
+});
